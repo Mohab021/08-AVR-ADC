f100ea8d47311e22ea5ee9d77e4d765b90d86599
Mohab Sharafeldin <100772670+Mohab021@users.noreply.github.com>
2022-03-12 13:38:00 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+
+#define F_CPU 4000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+#include "AVR_Ports.h"
+#include "AVR_LCD_Declarations.h"
+#include "AVR_ADC_Declarations.h"
+
+int main(void)
+{
+	uint16_t x;
+	char int_buffer[12]; //string for LCD printing
+	
+	LCD_Initialize();
+	ADC_Initialize();
+	
+	while(1)
+	{
+		x = ADC_Read(0); //read ADC value at PA0	
+		x = (x * 5) / (1023);
+		itoa(x, int_buffer, 10); //for printing Int, 10 for decimal output (as a string)
+		LCD_Print(int_buffer);
+		_delay_ms(1000);
+	}
+}
